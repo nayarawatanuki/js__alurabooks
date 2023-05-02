cbda5af11c88614bd3208fc018b9bc7c5f145548
nayarawatanuki <nayarawatanuki@gmail.com>
2023-05-01 23:18:17 -0300

    update `variables.js`

--- app/base/variable.js
+++ app/base/variable.js
@@ -1,10 +1,4 @@
-const zipCode = document.getElementById("zipCode");
-const messageErrorZipCode = document.getElementById("zipCode-error");
-
-const street = document.getElementById("street");
-const neighborhood = document.getElementById("neighborhood");
-const city = document.getElementById("city");
-const state = document.getElementById("state");
+const books = [];
 
 
-export { zipCode, messageErrorZipCode, street, neighborhood, city, state }
+export { books }
